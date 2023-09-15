59cd5d61f30b0e1c06072ac573263b173c9dfa67
Ifrah Malik <maliki1@tcnj.edu>
2023-09-15 01:00:31 -0400

    added code to handle student to event matching

--- pitchInFun.rb
+++ pitchInFun.rb
@@ -1,3 +1,4 @@
+
 =begin
 Ifrah Malik
 CSC415-01 Software Engineering
@@ -10,7 +11,7 @@ require 'csv'
 #initialzing hashmaps
 event = {}
 
-CSV.foreach("/home/student1/vm-csc415/assignment1/eventInput.csv", headers: true) do |row|
+CSV.foreach("/home/student1/vm-csc415/assignment1/events.csv", headers: true) do |row|
   event_id = row['Event-ID']
   event_name = row['Event-Name']
   event_issues = row['Event-Issues'].split('; ').map(&:strip)
@@ -36,6 +37,50 @@ CSV.foreach("/home/student1/vm-csc415/assignment1/studentInput.csv", headers: tr
   }
 end
 
-# Access the first record's values
+
 # puts event["4763"]
 # puts student["18673"]
+
+# Initialize a hash to store matching events for each student
+matching_events = {}
+
+# Iterate through each student
+student.each do |student_id, student_data|
+  student_interests = student_data['Interests']
+
+  # Initialize an array to store matching event IDs for this student
+  matching_event_ids = []
+
+  # Iterate through each event
+  event.each do |event_id, event_data|
+    event_issues = event_data['Event-Issues']
+
+    # Check if there's an intersection between event_issues and student_interests
+    if (event_issues & student_interests).any?
+      matching_event_ids << event_id
+    end
+  end
+
+  # Store the matching event IDs for this student
+  matching_events[student_id] = matching_event_ids
+end
+
+# Now, 'matching_events' contains the matching event IDs for each student
+matching_events.each do |student_id, event_ids|
+  student_data = student[student_id]
+  student_interests = student_data['Interests']
+
+  puts "Student ID: #{student_id}"
+  puts "Interests: #{student_interests.join(', ')}"
+  
+  if event_ids.empty?
+    puts "No matching events found for this student."
+  else
+    puts "Matching Event IDs: #{event_ids.join(', ')}"
+  end
+
+  puts "\n"  # Add a newline for separation
+end
+
+
+
